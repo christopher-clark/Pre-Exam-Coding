c7e5bfe2c824e59f84bfe87a7774e544c55ede0c
christopher-clark <g.kanizaj.clark@gmail.com>
2016-05-07 18:25:48 +1000

    Added Horse to Chapter 1

---
+++ Pre-Exam-Coding/src/chapter1/Horse.java
@@ -0,0 +1,18 @@
+package chapter1;
+
+public class Horse extends Animal{
+	public void eat() {
+		System.out.println("Horse Eating Hay and Oats");
+	}
+	public void eat(String s) {
+		System.out.println("Horse eating " + s);
+	}
+	public void eat(int i) {
+		System.out.println("Horse eating " + i + " bales of Hay");
+
+	}
+	public void eat(float f) {
+		System.out.println("Horse eating " + f + " bales of Hay");
+
+	}
+}

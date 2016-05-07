7c0abf8450fbcd5d426d89ea0623b2ed1b739354
christopher-clark <g.kanizaj.clark@gmail.com>
2016-05-07 18:32:54 +1000

    Added Dog

---
+++ Pre-Exam-Coding/src/chapter1/Dog.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class Dog extends Animal{
+	public void makeNoise(){
+		System.out.println("Woof Woof ");
+	}
+	public void eat() {
+		System.out.println("Woofer eating ");
+	}
+	public void woof() {
+		System.out.println("Woof Woof ");
+	}
+}

1ede1da6a20f7c4b6add646bf79d9c4182680f1e
christopher-clark <g.kanizaj.clark@gmail.com>
2016-05-07 18:05:18 +1000

    Committing Animal.java

---
+++ Pre-Exam-Coding/src/chapter1/Animal.java
@@ -0,0 +1,28 @@
+package chapter1;
+
+public class Animal {
+	String name;
+	 
+	Animal(String name) {
+	 this.name = name;
+	}
+	Animal(){
+		this(makeRandomName());
+	}
+	
+	static String makeRandomName() {
+		 String [] name = {"Fluffy", "Fido","Rover", "Spike","Gigi"};
+		 return name[(int) (Math.random() * name.length)];
+	}
+	public void makeNoise(){
+		System.out.println("Animal Noise ");
+	}
+	public void eat() {
+		System.out.println("Generic Animal Eating Generically");
+	}
+	public void eat(int i) {
+		System.out.println("Animal eating " + i + " pieces of Food");
+
+	}
+}
+

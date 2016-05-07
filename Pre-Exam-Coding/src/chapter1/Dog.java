762438c137a4903dd56d5d10f8a05cc6d32d916e
christopher-clark <g.kanizaj.clark@gmail.com>
2016-05-07 21:31:12 +1000

    Added new constructors

--- Pre-Exam-Coding/src/chapter1/Dog.java
+++ Pre-Exam-Coding/src/chapter1/Dog.java
@@ -1,6 +1,18 @@
 package chapter1;
 
 public class Dog extends Animal{
+	String Name;
+	int size;
+	
+	Dog(){
+		System.out.println("no-args constructor");
+	}
+	Dog(String name){
+		this.name = name;
+	}
+	Dog(int size){
+		this.size = size;
+	}
 	public void makeNoise(){
 		System.out.println("Woof Woof ");
 	}
